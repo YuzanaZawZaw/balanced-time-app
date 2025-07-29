409f00657210915d8a851742723a52cda2fba43b
YuzanaZawZaw <yuzanazawzaw7121998@gmail.com>
2025-07-29 15:54:42 +0630

    setup for Balanced Time App

---
+++ src/main/java/com/balancedtime/BalancedTimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.balancedtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancedTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalancedTimeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balancedtime/BalancedTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balancedtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalancedTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
